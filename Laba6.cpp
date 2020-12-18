6a63d4dfbea7f90970c3adac81048bec3d73f253
Polyushka <70941524+Polyushka@users.noreply.github.com>
2020-12-18 16:46:27 +0300

    Update Laba6.cpp

--- Laba6.cpp
+++ Laba6.cpp
@@ -7,21 +7,21 @@ int poisk(T *mas, T key,int size) {
 	for (int i = 0; i < size; i++)
 	{
 		if (mas[i] == key) return i+1;
-		i++;
 	}
 	return 0;
 }
 int poisk(char** mas,char* str,int size) {
-	int j = 0, flg = 0,count =0;
+	int j = 0;
 	for (int i = 0; i < size; i++)
-	{
-		while (!mas[size][j] || !str[j])
+	{int flg = 0,count =0;
+		while (mas[i][j] && str[j])
 		{
 			count++;
-			if (mas[size][j] = str[j])
+			if (mas[i][j] == str[j])
 				flg++;
+			j++;
 		}
-		if (flg == count)return i+1;
+		if (flg == count && flg!=0)return i+1;
 	}
 }
 template<typename T>
@@ -31,54 +31,68 @@ template<typename T>
 			std::cout << std::endl;
 	}
 	void output(char**ms, int size) {
+		std::cout << std::endl;
 		for (int i = 0; i < size; i++)
 			std::cout << ms[i] << std::endl;
 	}
 	int main() {
 		system("chcp 1251> null");
-		std::cout << "Выберите тип данных с которым хотите работать : \n1-int\n2-float\n3-char*" << std::endl;
-		int answ = input_int(0,4);
-		switch (answ-1) {
-		case 0: {
-			int s1 = input_int(0, 10);
-			int* masi;
-		masi = new int[s1];
-		for (int i = 0; i < s1; i++) {
-			masi[i] = input_int(0, 1000);
-		}
-		output(masi, s1);
-		int ikei = input_int(0, 1000);
-		int g = poisk(masi, ikei, s1);
-		std::cout << g << std::endl;
-		delete[]masi; break;
-		}
-		case 1: {int s2 = input_int(0, 10);
-			float *masf;
-		masf = new float[s2];
-		for (int i = 0; i < s2; i++) {
-			masf[i] = input_float(0, 1000);
-		}
-		output(masf, s2);
-		float fkey = input_float(0,1000);
-		int g=poisk(masf,fkey,s2);
-		std::cout << "\n" << g;
-		delete[]masf;
-		break; }
-		case 2: {int s3 = input_int(0, 10);
+		while (1) {
+			std::cout << "Выберите тип данных с которым хотите работать : \n1-int\n2-float\n3-char*" << std::endl;
+			int answ = input_int(0, 3);
+			switch (answ - 1) {
+			case 0: {
+				int s1 = input_int(0, 10);
+				int* masi;
+				masi = new int[s1];
+				std::cout << "Введите элементы массива int\n";
+				for (int i = 0; i < s1; i++) {
+					masi[i] = input_int(0, 1000);
+				}
+				output(masi, s1);
+				std::cout << "Введите значение для поиска\n";
+				int ikei = input_int(0, 1000);
+				int g = poisk(masi, ikei, s1);
+				std::cout << "Искомое значение - <<"<< g <<"-ый элемент массива"<<std::endl;
+				delete[]masi; break;
+			}
+			case 1: {int s2 = input_int(0, 10);
+				float *masf;
+				std::cout << "Введите элементы массива float\n";
+				masf = new float[s2];
+				for (int i = 0; i < s2; i++) {
+					masf[i] = input_float(0, 1000);
+				}
+				output(masf, s2);
+				std::cout << "Введите значение для поиска\n";
+				float fkey = input_float(0, 1000);
+				int g = poisk(masf, fkey, s2);
+				std::cout << "Искомое значение - <<" << g << "-ый элемент массива" << std::endl;
+				delete[]masf;
+				break; }
+			case 2: {int s3 = input_int(0, 10);
 				char** ms;
-				ms = new char*[s3]; 
+				ms = new char*[s3];
 				std::cin.clear();
 				std::cin.ignore(std::cin.rdbuf()->in_avail());
+				std::cout << "Введите элементы массива char*\n";
 				for (int i = 0; i < s3; i++)
-				{ms[i] = new char[10];
-				std::cin >> ms[i];
+				{
+					ms[i] = new char[10];
+					std::cin >> ms[i];
 				}
+				std::cout << "Введите значение для поиска\n";
 				char* ckey = new char[10];
 				std::cin >> ckey;
-				int c = poisk(ms, ckey, s3);
-		output(ms, s3);
-		delete[]ms;
-		break; }
+				int g = poisk(ms, ckey, s3);
+				std::cout << "Искомое значение - " << g << "-ый элемент массива" << std::endl;
+				output(ms, s3);
+				delete[]ms;
+				break; }
+			}
+			std::cout << "/nХотите продолжить? (Да - 1,Нет - 0)" << std::endl;
+			int a= input_int(0, 1);
+				if (a == 0)break;
 		}
 		return 0;
 	}
